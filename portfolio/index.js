9dfe38d9f321f9ba14ea9ac6526e17d06ecab703
AlexiyKami <Leksij01@gmail.com>
2022-01-21 16:47:36 +0300

    feat: add adaptive menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,16 @@
 console.log("Score: 110 \nValidator: + 10\nSemantic layout: + 20\nLayout matches pattern: + 48\nCSS requirements: + 12\nInteractivity: + 20");
 
-function changeIcon(x) {
-    x.classList.toggle("change");
-  }
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu ul li a');
+
+function toggleMenu() {
+  hamburger.classList.toggle('change');
+  menu.classList.contains('menu') ? menu.classList.remove('menu') : menu.classList.add('menu');
+  menu.classList.toggle('active');
+}
+
+menuLinks.forEach((i) => i.addEventListener('click',toggleMenu));
+
+hamburger.addEventListener('click', toggleMenu);
+
