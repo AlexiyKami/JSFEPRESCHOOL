25f34e017127f700085634a4027634fea356e59e
AlexiyKami <Leksij01@gmail.com>
2022-01-26 19:22:09 +0300

    feat: add portfolio images switching

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,5 @@
 console.log("Score: 85 \nLayout matches pattern(768px): + 48\n>320px has no horizontal scroll bar: + 15\nAdaptive menu: + 22");
-
+// -------------hamburger menu-------------
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.menu ul li a');
@@ -14,3 +14,30 @@ menuLinks.forEach((i) => i.addEventListener('click',toggleMenu));
 
 hamburger.addEventListener('click', toggleMenu);
 
+
+// -------------portfolio-images-------------
+
+const portfolioBtns = document.querySelector('.portfolio-btns');
+const portfolioImages = document.querySelectorAll('.portfolio-img');
+
+function changeImage(event) {
+  if(event.target.classList.contains('portfolio-btn')) {
+    portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
+  }
+}
+
+portfolioBtns.addEventListener("click", changeImage);
+
+// -------------preloader-------------
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+function preloadSummerImages() {
+  seasons.forEach((season,index) => {
+    for (let i=0; i < portfolioImages.length;i++) {
+      let img = new Image();
+      img.src = `./assets/img/${season}/${i + 1}.jpg`
+      console.log(img);
+    }
+  });
+}
+preloadSummerImages();
