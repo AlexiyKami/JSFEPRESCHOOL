086f9581ae870538e65d1215c4803e6b3f548306
Alex <AlexiyKami@github.com>
2022-02-10 20:51:26 +0300

    feat: add popular button and cross for clear form

--- movie-app/index.js
+++ movie-app/index.js
@@ -1,10 +1,37 @@
 import i180bj from './translate.js';
 
+console.log(`Score: 70
+Вёрстка: +10;
+При загрузке приложения на странице отображаются карточки фильмов с полученными от API данными: +10;
+Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся карточки фильмов, в названиях которых есть это слово, если такие данные предоставляет API: +10;
+Поиск: +30;
+Очень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения: +10;
+Добавлены:
+- меню с описанием и ссылкой на фильм в Google;
+- статистика запроса: "по запросу (х) найдено (y) фильмов";
+- раздел: популярное;
+- переключение языка en\ru;
+- рейтинг фильма в правом верхнем углу карточки;
+- жанры;
+- переключение страниц на следующую и предыдущую;
+`);
+
 let language = 'en';
 let query = '';
 let page = 1;
-let url = `https://api.themoviedb.org/3/search/movie?api_key=f72149d6bc624d0a624f793a656c6f1a&query=${query}&page=${page}&language=${language}`;
-let genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=f72149d6bc624d0a624f793a656c6f1a&language=${language}`;
+let url = '';
+let genreUrl = '';
+let searchMode;
+
+const popularBtn = document.querySelector('.popular-btn');
+popularBtn.addEventListener('click', switchMode);
+function switchMode() {
+    searchMode = 'false';
+    localStorage.setItem('searchMode',searchMode);
+    page = 1;
+    localStorage.setItem('page',page);
+    location.reload();
+}
 
 const languageBtn = document.querySelector('.switch-lng');
 languageBtn.addEventListener('click', switchLng);
@@ -18,9 +45,12 @@ function switchLng() {
     location.reload();
 }
 
+
 const form = document.getElementById('search');
-form/addEventListener('input', getInputValue);
+form.addEventListener('input', getInputValue);
 function getInputValue() {
+    searchMode = 'true';
+    localStorage.setItem('searchMode',searchMode);
     query = form.value;
     localStorage.setItem('query',query);
     page = 1;
@@ -30,15 +60,46 @@ function getInputValue() {
 const pageNum = document.querySelector('.page-num');
 
 function getLocalStorage() {
-    query = localStorage.getItem('query');
-    language = localStorage.getItem('language');
-    page = localStorage.getItem('page');
+    if(localStorage.getItem('query')) {
+        query = localStorage.getItem('query');
+    }
+
+    if(language = localStorage.getItem('language')) {
+        language = localStorage.getItem('language');
+    } else language = "en";
+
+    if(localStorage.getItem('page')) {
+        page = localStorage.getItem('page');
+    }
+    if(localStorage.getItem('searchMode')) {
+        searchMode = localStorage.getItem('searchMode');
+    }
+
     pageNum.textContent = page;
     form.placeholder = query;
+
+    const crossBtn = document.querySelector('.cross-btn');
+    crossBtn.addEventListener('click', function clearForm() {
+        form.placeholder = i180bj[language][form.dataset.i18];
+        query = "";
+        form.value = "";
+    }
+    )
+
+    if(form.placeholder == "") {
+        form.placeholder = i180bj[language][form.dataset.i18];
+    }
+        
+
     const langElements = document.querySelectorAll('[data-i18]');
     langElements.forEach(elem => elem.textContent = i180bj[language][elem.dataset.i18]);
-    url = `https://api.themoviedb.org/3/search/movie?api_key=f72149d6bc624d0a624f793a656c6f1a&query=${query}&page=${page}&language=${language}`;
     genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=f72149d6bc624d0a624f793a656c6f1a&language=${language}`;
+    if(searchMode == 'true') {
+        url = `https://api.themoviedb.org/3/search/movie?api_key=f72149d6bc624d0a624f793a656c6f1a&query=${query}&page=${page}&language=${language}`;
+    } else {
+        url = `https://api.themoviedb.org/3/movie/popular?api_key=f72149d6bc624d0a624f793a656c6f1a&language=${language}&page=${page}`
+    }
+
     getData();
 }
 window.addEventListener('load',getLocalStorage);
@@ -49,30 +110,36 @@ async function getData() {
     const data = await res.json();
     const genreData = await genreRes.json();
     showData(data,genreData);
-    console.log(data);
 }
 
 function showData(data, genreData) {
     const cardWrapper = document.querySelector('.card-wrapper');
     const searchStat = document.querySelector('.search-stat');
     const totalResults = data.total_results;
-    if(language == 'ru') {
-        searchStat.textContent = `По запросу '${query}' найдено ${totalResults} фильмов`;
+    if(searchMode == 'true') {
+        if(language == 'ru') {
+            searchStat.textContent = `По запросу '${query}' найдено ${totalResults} фильмов`;
+        } else {
+            searchStat.textContent = `'${query}' : found ${totalResults} movies`;
+        }
+        
+        if(query == '' && language == 'en') {
+            searchStat.textContent = `Empty query`;
+        } 
+        if(query == '' && language == 'ru') {
+            searchStat.textContent = `Пустой запрос`;
+        } 
+    } else if(language == 'ru') {
+        searchStat.textContent = `Популярные фильмы`;
     } else {
-        searchStat.textContent = `'${query}' : found ${totalResults} movies`;
+        searchStat.textContent = `Popular films`;
     }
     
-    if(query == '' && language == 'en') {
-        searchStat.textContent = `Empty query`;
-    } 
-    if(query == '' && language == 'ru') {
-        searchStat.textContent = `Пустой запрос`;
-    } 
     data.results.map(elem => {
         const card = `<div class="card-item">
         <p class="card-rating">${elem.vote_average.toFixed(1)}</p>
         <div class="card-img-wrapper">
-            <img src="https://image.tmdb.org/t/p/w300${elem.poster_path}" alt="image" class="card-img">
+            <img src="${getImage(elem)}" alt="image" class="card-img">
         </div>
         <div class="card-info">
             <h3>${elem.title} ${getDate(elem)}</h3>
@@ -81,7 +148,8 @@ function showData(data, genreData) {
             <p>${getGenre(elem)}</p>
         </div>
         <div class="card-menu">
-                    <p>${elem.overview}</p>
+                    <p>${getDescription(elem)}</p>
+                    <a href="https://www.google.com/search?q=${elem.title} ${getDate(elem)}" target="_blank">${language == 'ru' ? "Открыть в Google" : "Open in Google"}</a>
                 </div>
         </div>`;
         cardWrapper.insertAdjacentHTML('beforeend', card);
@@ -143,6 +211,24 @@ function showData(data, genreData) {
             return '(' + elem.release_date.substring(0,4) + ')';
         } else return '';
         
+    }
+
+    function getImage(elem) {
+        if(elem.poster_path) {
+            return `https://image.tmdb.org/t/p/w300${elem.poster_path}`
+        } else {
+            return 'assets/img/noimage.jpg'
+        }
+    }
+
+    function getDescription(elem) {
+        if(elem.overview == "" && language == "en") {
+            return "No description"
+        } 
+        if(elem.overview == "" && language == "ru") {
+            return "Нет описания"
+        }
+        return elem.overview;
     }
 }
 
--- movie-app/translate.js
+++ movie-app/translate.js
@@ -3,13 +3,17 @@ const i18Obj = {
       'switch-lng': 'Switch to Russian',
       'prev': 'previous',
       'next' : 'next',
-      'search-movie' : 'Search Movie'
+      'search-movie' : 'Search Movie',
+      'popular-btn' : 'Popular',
+      'search' : 'Search'
     },
     'ru': {
       'switch-lng': 'Переключить на английский',
       'prev': 'предыдущая',
       'next' : 'следующая',
-      'search-movie' : 'Поиск фильмов'
+      'search-movie' : 'Поиск фильмов',
+      'popular-btn' : 'Популярное',
+      'search' : 'Найти'
     }
   }
   export default i18Obj;
